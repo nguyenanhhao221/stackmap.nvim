5f1fc4ca452c78228017302e277c749ff9ad71a2
Hao Nguyen <hao@haonguyen.tech>
2023-05-28 18:15:10 +0700

    test(scope): add test for multiple pushes

--- tests/stackmap_spec.lua
+++ tests/stackmap_spec.lua
@@ -14,12 +14,25 @@ describe("stackmap", function()
   it("can push a single mapping", function()
     local rhs = "echo 'This is a test'"
 
-    require("stackmap").push("test", "n", {
-      ["assjkadhksa"] = rhs,
-    })
+    require("stackmap").push("test", "n", { ["assjkadhksa"] = rhs })
 
     local maps = vim.api.nvim_get_keymap("n")
     local found = find_map(maps, "assjkadhksa")
     assert.are.same(rhs, found.rhs)
+  end)
+
+  it("can push a multiple mappings", function()
+    local rhs = "echo 'This is a test'"
+
+    require("stackmap").push("test 2", "n", {
+      ["aaaa"] = rhs .. "1",
+      ["bbbb"] = rhs .. "2",
+    })
+
+    local maps = vim.api.nvim_get_keymap("n")
+    local found_1 = find_map(maps, "aaaa")
+    assert.are.same(rhs .. "1", found_1.rhs)
+    local found_2 = find_map(maps, "bbbb")
+    assert.are.same(rhs .. "2", found_2.rhs)
   end)
 end)
