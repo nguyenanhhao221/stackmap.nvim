3d1d51ce9dd3354ded2bdcf184628c117ab435c9
Hao Nguyen <hao@haonguyen.tech>
2023-12-08 22:12:38 +0700

    feat: basic remap works

--- lua/stackmap/init.lua
+++ lua/stackmap/init.lua
@@ -1,7 +1,48 @@
 local M = {}
 
-M.setup = function(opts)
-  print("Options:", opts)
+M._stack = {}
+
+---@param maps table: mapping table
+---@param lhs string: left hand side mapping
+local find_mapping = function(maps, lhs)
+  for _, value in ipairs(maps) do
+    if value.lhs == lhs then
+      return value
+    end
+  end
 end
+
+---
+---@param name string
+---@param mode string
+---@param mapping table<string,string>
+M.push = function(name, mode, mapping)
+  local maps = vim.api.nvim_get_keymap(mode)
+  local existing_maps = {}
+
+  for lhs, rhs in pairs(mapping) do
+    local existing = (find_mapping(maps, lhs))
+    if existing then
+      table.insert(existing_maps, existing)
+    end
+  end
+
+  M._stack[name] = existing_maps
+  for lhs, rhs in pairs(mapping) do
+    -- TODO: need some way to pass options in here
+    vim.keymap.set(mode, lhs, rhs)
+  end
+end
+
+M.pop = function(name) end
+
+-- lua require("mapstack").push("debug_mode", {...})
+
+-- lua require("mapstack").pop("debug_mode")
+
+M.push("debug_mode", "n", {
+  [" st"] = "echo 'Hello'",
+  [" sp"] = "echo 'Goodbye'",
+})
 
 return M
